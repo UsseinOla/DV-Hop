9a9506f5bc6e0c345981450180b6a5a8b5c50032
UsseinOla <77444770+UsseinOla@users.noreply.github.com>
2022-01-01 14:44:35 -0500

    Update node.c

--- DV_HOP/Cooja Implimentation For DV_Hop/node.c
+++ DV_HOP/Cooja Implimentation For DV_Hop/node.c
@@ -16,14 +16,17 @@
 #define anchors_num 3
 #define MAX_NEIGHBORS 16
 char buffer[80];
+
 /* This structure holds information about neighbors. */
-struct neighbor {
+struct neighbor
+{
 struct neighbor *next;
 rimeaddr_t addr;
-};
-rimeaddr_t next_hop; uint8_t nbr_hop;
+};rimeaddr_t next_hop; uint8_t nbr_hop;
+
 /* This structure holds information about database. */
-struct database {
+struct database
+{
 uint8_t type;
 uint8_t  id;
 float  x;
@@ -36,16 +39,16 @@ struct database *received_data_mote;
 struct database routing_table[anchors_num];
 int counter_routing_one=0;
 
-struct average_hop_size {
+struct average_hop_size
+{
 uint8_t type;
 uint8_t  id;
 float Av_Hop_Size;
 }average_hop_size;
+
 struct average_hop_size *received_avh_data;
-
-
-
-struct location {
+struct location 
+{
 uint8_t id;
 float x;
 float y;
@@ -70,19 +73,25 @@ PROCESS(trilateral_process, "trilateral process");
    start when this module is loaded. We put both our processes
    there. */
 AUTOSTART_PROCESSES(&broadcast_process, &unicast_process);
+
 /*---------------------------------------------------------------------------*/
 /* This function is called whenever a broadcast message is received. */
 static void broadcast_recv(struct broadcast_conn *c, const rimeaddr_t *from)
 { 
 received_data_mote = packetbuf_dataptr();
 /*  Registration of database information in the databases table */ 
-if(received_data_mote->type==0){
+if(received_data_mote->type==0)
+{
 int i=0; 
-for(i=0;i<counter_routing_one;i++){
-if (received_data_mote->id==routing_table[i].id){
-if(received_data_mote->hop_count <routing_table[i].hop_count){
+for(i=0;i<counter_routing_one;i++)
+{
+if (received_data_mote->id==routing_table[i].id)
+{
+if(received_data_mote->hop_count <routing_table[i].hop_count)
+{
 routing_table[i].hop_count=received_data_mote->hop_count; 
-if(received_data_mote->id==1){
+if(received_data_mote->id==1)
+{
 rimeaddr_copy(&next_hop, from);
 nbr_hop=received_data_mote->hop_count;
 }
@@ -94,20 +103,26 @@ break;
 break;              
 }
 } 
-if(i==counter_routing_one){
+if(i==counter_routing_one)
+{
 routing_table[counter_routing_one]=*received_data_mote; counter_routing_one++;
-if(received_data_mote->id==1){
+if(received_data_mote->id==1)
+{
 rimeaddr_copy(&next_hop, from);
-nbr_hop=received_data_mote->hop_count;    }
+nbr_hop=received_data_mote->hop_count;
+}
 process_start(&display_process, NULL);
 process_start(&flooding_process, NULL);i=0;
 }
 }
-else if(received_data_mote->type==1){
+else if(received_data_mote->type==1)
+{
 received_avh_data= packetbuf_dataptr();
 int i;
-for(i=0;i<counter_routing_one;i++){
-if ((received_avh_data->id==routing_table[i].id)&&(routing_table[i].Av_Hop_Size==0)){
+for(i=0;i<counter_routing_one;i++)
+{
+if ((received_avh_data->id==routing_table[i].id)&&(routing_table[i].Av_Hop_Size==0))
+{
 routing_table[i].Av_Hop_Size=received_avh_data->Av_Hop_Size; ids_counter++;
 process_start(&display_process, NULL);
 process_start(&flooding_process, NULL);
@@ -115,13 +130,13 @@ break;
 }
 }
 }
-/*************************************************************/
 }
 
 /* This is where we define what function to be called when a broadcast
    is received. We pass a pointer to this structure in the
    broadcast_open() call below. */
 static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
+
 /*---------------------------------------------------------------------------*/
 /* This function is called for every incoming unicast packet. */
 static void
@@ -132,6 +147,7 @@ packetbuf_copyfrom(received_location, sizeof(struct location));
 unicast_send(&unicast, &next_hop);
 }
 static const struct unicast_callbacks unicast_callbacks = {recv_uc};
+
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(broadcast_process, ev, data)
 {
@@ -150,11 +166,14 @@ static struct etimer et1;
 PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
 PROCESS_BEGIN();
 broadcast_open(&broadcast, 129, &broadcast_call);
-if(received_data_mote->type==0){
+if(received_data_mote->type==0)
+{
 received_data_mote->hop_count++;
 }
-else if(received_data_mote->type==1){
-if(ids_counter==3){
+else if(received_data_mote->type==1)
+{
+if(ids_counter==3)
+{
 etimer_set(&et1, (CLOCK_SECOND)*2+random_rand() % (CLOCK_SECOND)*2);
 PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et1));   
 process_start(&trilateral_process, NULL);
@@ -162,6 +181,7 @@ process_start(&trilateral_process, NULL);
 }
 PROCESS_END();
 }
+
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(unicast_process, ev, data)
 {
@@ -170,6 +190,7 @@ PROCESS_BEGIN();
 unicast_open(&unicast, 146, &unicast_callbacks);
 PROCESS_END();
 }
+
 /*-----------------------------------------------------------------------------------*/
  PROCESS_THREAD(trilateral_process, ev, data)
 {
@@ -177,8 +198,8 @@ static struct etimer et1;
 PROCESS_EXITHANDLER(unicast_close(&unicast);)
 PROCESS_BEGIN();
 unicast_open(&unicast, 146, &unicast_callbacks);
+    
 /****************Calcule Matrix A:(anchors_num-1*2)****************/
-
  int l=0,l1=0;
 int dec_x; float frac_x;
 
@@ -193,7 +214,6 @@ A=(float**)malloc((anchors_num-1)*sizeof(float*));
     *(*(A+i)+j)=-2*(routing_table[i].x-routing_table[anchors_num-1].x);
   else if (j==1)
     *(*(A+i)+j)=-2*(routing_table[i].y-routing_table[anchors_num-1].y);
-
 
 /****************Calcule Matrix B:(nbr_received_data_mote-1*1)*****************/
    float **B;float eval=0;
@@ -240,19 +260,19 @@ for(i=0;i<anchors_num;i++)
 free(A[i]);
 free(A);
 
-
 for(i=0;i<anchors_num;i++)
 free(B[i]);
 free(B);
-
 free(A_T);
 free(ATA);
 free(ATA_Inv);
 free(ATA_AT);
 free(P);
+    
 PROCESS_EXIT();
 PROCESS_END();
  }
+
 /*---------------------------------------------------------------------------*/
  PROCESS_THREAD(blink_process, ev, data)
  {
@@ -264,12 +284,14 @@ PROCESS_END();
  leds_off(LEDS_GREEN);     
  PROCESS_END();
  }
+
 /*---------------------------------------------------------------------------*/
  PROCESS_THREAD(display_process, ev, data)
 {
 PROCESS_BEGIN(); 
 int j=0;
-for(j=0;j<counter_routing_one;j++) {
+for(j=0;j<counter_routing_one;j++)
+{
 int dec = routing_table[j].Av_Hop_Size;
 float frac = routing_table[j].Av_Hop_Size - dec;
 printf("mote=%u,Nbr_hop=%u,Av_Hop_Size=%d.%02d/",routing_table[j].id,routing_table[j].hop_count,dec,abs((int)(frac * 100)));
@@ -277,4 +299,3 @@ printf("mote=%u,Nbr_hop=%u,Av_Hop_Size=%d.%02d/",routing_table[j].id,routing_tab
 printf("\n");
 PROCESS_END();
 }
-/*---------------------------------------------------------------------------*/
